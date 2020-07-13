f6172aca8559b83f1627cb53924336104081ac1b
edwardsuwirya <edward.suwirya@ffi.co.id>
2020-07-13 09:54:52 +0700

    [UPDATE] Extension Function Topic add more example

--- src/main/kotlin/com/enigmacamp/topic/KB014-ExtensionFunction.kt
+++ src/main/kotlin/com/enigmacamp/topic/KB014-ExtensionFunction.kt
@@ -1,7 +1,16 @@
 package com.enigmacamp.topic
 
+//Menambahkan kemampuan baru dari sebuah object
 fun String.hello(): String {
     return "Hello $this"
+}
+
+fun Int.genap(): Int? {
+    if (this % 2 == 0) {
+        return this
+    } else {
+        return null
+    }
 }
 
 fun main() {
@@ -9,4 +18,11 @@ fun main() {
     println(name.hello())
 
     println("Sasa".hello())
+
+    val myNumbers = arrayOf(1, 2, 3, 4, 5, 6, 7)
+    for (n in myNumbers) {
+        if (n.genap() != null) {
+            println(n)
+        }
+    }
 }
